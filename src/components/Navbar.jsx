4e3bc9fb416d5644e8253d3a08106a2026ca5c57
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 11:50:01 +0200

    Add dropdown menu icon and styles to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="w-screen h-[80px] z-10 bg-zinc-300 drop-shadow-lg fixed">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center">
+            
+        </div>
+      </div>
+      <MenuIcon className='w-6'/>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
