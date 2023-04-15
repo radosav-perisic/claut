e518d24f742fa55553756226b58b7b89f065432c
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 12:35:42 +0200

    Create Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {CloudUploadIcon, DatabaseIcon, PaperAirplaneIon, ServerIcon} from '@heroicons/react/solid'
+import background from '../assets/security.png'
+
+const Hero = () => {
+  return 
+  (
+    <div className='w-full h-screen bg-gray-200 flex flex-col justify-between'>Hero</div>
+  )
+}
+
+
+export default Hero
