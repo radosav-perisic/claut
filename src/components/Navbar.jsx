ee328e1d94b72ed665374ec2b04b2a2671ab9457
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 12:05:48 +0200

    Add navigation links to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,13 @@ const Navbar = () => {
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
             <h1 className="text-3xl font-bold mr-4 sm:text-4xl ">Claut.</h1>
+            <ul className="hidden md:flex ">
+             <li>Home</li>
+             <li>About</li>
+             <li>Support</li>
+             <li>Platforms</li>
+             <li>Pricing</li>
+            </ul>
         </div>
       </div>
       <MenuIcon className='w-6'/>
