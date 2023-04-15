ec7eb22bbc8bce9ef6f4b188503b837a3fb7633b
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 13:04:17 +0200

    Add About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Navbar from "./components/Navbar"
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
-
   return (
     <div>
-    <Navbar />
+      <Navbar />
+      <Hero />
+      <About />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
