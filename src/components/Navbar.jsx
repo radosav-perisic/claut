0e46fcd54508d1a996834978a88ac413c8d408fd
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 11:57:57 +0200

    Add Navbar Title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="w-screen h-[80px] z-10 bg-zinc-300 drop-shadow-lg fixed">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-            
+            <h1 className="text-3xl font-bold mr-4 sm:text-4xl ">Claut.</h1>
         </div>
       </div>
       <MenuIcon className='w-6'/>
