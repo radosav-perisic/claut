920e1f283771e0e9be8f3849b7cbaab1cc2bbf5d
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 12:45:14 +0200

    Implement new branding elements in Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import {CloudUploadIcon, DatabaseIcon, PaperAirplaneIon, ServerIcon} from '@heroicons/react/solid'
-import background from '../assets/security.png'
+import React from 'react';
+import {CloudUploadIcon, DatabaseIcon, PaperAirplaneIcon, ServerIcon} from '@heroicons/react/solid';
+import background from '../assets/security.png';
 
 const Hero = () => {
-  return 
-  (
-    <div className='w-full h-screen bg-gray-200 flex flex-col justify-between'>Hero</div>
+  return ( 
+    <div className='w-full h-screen bg-gray-200 flex flex-col justify-between'>
+        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+            <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+                <p className='text-2xl'>Sequencing & Production with a Distinctive Approach</p>
+                <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
+                <p  className='2xl'>Introducing Our Innovative Tech Brand</p>
+                <button className='py-3 px-6 sm:w-[60%]'>Get Started</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
