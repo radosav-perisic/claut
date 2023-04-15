2fdd70cf2a30c04519992e0bb3836a3663a5a8ac
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-15 13:13:47 +0200

    Add Trust-Building Elements to About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='w-full my-32'>
+     <div className='max-w-[1240px] mx-auto'>
+        <div className='text-center'>
+            <h2 className='text-5xl font-bold'>We've Earned the Trust of Developers Worldwide</h2>
+            <p className='text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        </div>
+        <div>
+            <div>
+                <p>100%</p>
+                <p>Completion</p>
+            </div>
+            <div>
+                <p>24/7</p>
+                <p>Accessibility</p>
+            </div>
+            <div>
+                <p>100K</p>
+                <p>Transactions</p>
+            </div>
+        </div>
+     </div>
+    </div>
   )
 }
 
