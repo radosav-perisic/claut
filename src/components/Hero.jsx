8b171203fa1283035c9c630d7cc990e7fc7c2de2
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-16 09:46:13 +0200

    Add duration to Get Started btn hover in Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Cloud Management
           </h1>
           <p className="2xl">Introducing Our Innovative Tech Brand</p>
-          <button className="py-3 px-6 sm:w-[60%] my-4">Get Started</button>
+          <button className="py-3 px-6 sm:w-[60%] my-4 duration-300">Get Started</button>
         </div>
         <div>
           <img className="w-full" src={background} alt="/" />
