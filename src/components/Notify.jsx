37648ac1b4e697ddb39aa93318cef4c50d9f20cb
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-16 12:00:14 +0200

    Add All-Inclusive section with Notifications

--- src/components/Notify.jsx
+++ src/components/Notify.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import { CheckIcon } from "@heroicons/react/outline";
+import React from "react";
 
 const Notify = () => {
   return (
-    <div>Notify</div>
-  )
-}
+    <div className="w-full my-32">
+      <div className="max-w-[1240px] mx-auto px-2">
+        <h2 className="text-5xl font-bold text-center">All-Inclusive Platform</h2>
+        <p className="text-2xl py-8 text-gray-500 text-center"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+      <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+            <div className="flex"> 
+            <div>
+                <CheckIcon className="w-7 mr-4 text-green-600"/>
+                </div>
+                <div>
+                    <h3 className="font-bold text-lg">Notifications</h3>
+                    <p className="text-lg pt-2 pb-4"> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                </div>
+            </div>
+        </div>
+      </div>
+      </div>
+  );
+};
 
-export default Notify
+export default Notify;
