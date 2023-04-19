974e4413b0ba9f8b6424cf6b42bdfa5368ebca72
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-19 14:27:20 +0200

    Update icon placement and spacing in Pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -4,7 +4,7 @@ import { CheckIcon } from "@heroicons/react/solid";
 const Pricing = () => {
   return (
     <div className="w-full text-white my-24">
-      <div className="w-full h-[800px] bg-slate-900 absolute mix-blend-overlay">Absolute</div>
+      <div className="w-full h-[800px] bg-[#140e25] absolute mix-blend-overlay">
       <div className="max-w-[1240px] mx-auto py-12">
         <div className="text-center py-8 text-slate-300">
           <h2 className="text-3xl uppercase">Pricing</h2>
@@ -16,23 +16,41 @@ const Pricing = () => {
             dignissimos numquam quasi repudiandae earum iusto.
           </p>
         </div>
-
-        <div className="grid md:grid-col-2 bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl">
-            <span className="py-1 px-3 uppercase bg-indigo-200 text-indigo-900 rounded-2xl text-sm">Standard</span>
+<div className="grid md:grid-cols-2">
+        <div className=" bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+            <span className="py-1 px-3 uppercase bg-violet-200 text-purple-900 rounded-2xl text-sm">Standard</span>
             <div>
                 <p className="text-6xl flex py-4 font-bold">$38<span className="flex flex-col text-xl text-slate-500 justify-end">/mo</span></p>
             </div>
             <p className="text-2xl py-8 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-            <div>
-                <p><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
-                <p><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
-                <p><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
-                <p><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
-                <p><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
-                <button>Subscribe</button>
+            <div className="text-2xl relative">
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <button className="w-full py-4 my-4 duration-300">Subscribe</button>
             </div>
         </div>
+        <div className=" bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
+            <span className="py-1 px-3 uppercase bg-violet-200 text-purple-900 rounded-2xl text-sm">Premium</span>
+            <div>
+                <p className="text-6xl flex py-4 font-bold">$78<span className="flex flex-col text-xl text-slate-500 justify-end">/mo</span></p>
+            </div>
+            <p className="text-2xl py-8 text-slate-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div className="text-2xl relative">
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <p className="flex py-4 "><CheckIcon className="w-8 text-green-800 mr-5"/>Lorem, ipsum dolor</p>
+                <button className="w-full py-4 my-4 duration-300">Subscribe</button>
+            </div>
+        </div>
+        </div>
+        
       </div>
+    </div>
     </div>
   );
 };
