748849c281b04646b279727bd3c5f5f7e0d29cf9
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-19 13:30:35 +0200

    Add responsive grid & change font sizes & colors

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,18 +5,19 @@ const Pricing = () => {
   return (
     <div className="w-full text-white my-24">
       <div className="w-full h-[800px] bg-slate-900 absolute mix-blend-overlay">Absolute</div>
-      <div>
-        <div>
-          <h2>Pricing</h2>
-          <h3>Find the perfect price for your research</h3>
-          <p>
+      <div className="max-w-[1240px] mx-auto py-12">
+        <div className="text-center py-8 text-slate-300">
+          <h2 className="text-3xl uppercase">Pricing</h2>
+          <h3 className="text-5xl font-bold text-white py-8">Find the perfect price for your research</h3>
+          <p className="text-3xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad iure
             quod voluptate inventore fugit fuga perferendis exercitationem
             voluptatibus consequatur. Harum quisquam est labore, possimus
             dignissimos numquam quasi repudiandae earum iusto.
           </p>
         </div>
-        <div>
+
+        <div className="grid md:grid-col-2 bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl">
           <div>
             <span>Standard</span>
             <div>
