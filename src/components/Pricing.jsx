ca9f6dbe850b0d2ff11c8b05681b51cfdeb8cfde
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-19 16:27:29 +0200

    Fix Footer overlapping Pricing component
    Fix footer overlapping issue by removing unnecessary div wrapper.

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -3,8 +3,8 @@ import { CheckIcon } from "@heroicons/react/solid";
 
 const Pricing = () => {
   return (
-    <div className="w-full text-white my-24">
-      <div className="w-full h-[800px] bg-[#140e25] absolute mix-blend-overlay">
+    <div name='pricing' className="w-full text-white my-24">
+      <div className="w-full h-[800px] bg-[#140e25] absolute mix-blend-overlay"></div>
       <div className="max-w-[1240px] mx-auto py-12">
         <div className="text-center py-8 text-slate-300">
           <h2 className="text-3xl uppercase">Pricing</h2>
@@ -16,6 +16,7 @@ const Pricing = () => {
             dignissimos numquam quasi repudiandae earum iusto.
           </p>
         </div>
+        
 <div className="grid md:grid-cols-2">
         <div className=" bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative">
             <span className="py-1 px-3 uppercase bg-violet-200 text-purple-900 rounded-2xl text-sm">Standard</span>
@@ -50,7 +51,6 @@ const Pricing = () => {
         </div>
         
       </div>
-    </div>
     </div>
   );
 };
