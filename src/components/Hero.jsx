85cabd8880996e849a5089c0eaa834b4ae6e96c1
Radosav Perisic <radosavperisic00@gmail.com>
2023-04-22 09:28:22 +0200

    Improve mobile responsiveness of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
   return (
     <div name='home' className="w-full h-screen bg-gray-200 flex flex-col justify-between">
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
-        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8 mt-1">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8 lg:mt-1 mt-16">
           <p className="text-2xl font-medium">
             Sequencing & Production with a Distinctive Approach
           </p>
@@ -26,19 +26,19 @@ const Hero = () => {
         <div>
           <img className="w-full" src={background} alt="/" />
         </div>
-        <div className="absolute mt-20 lg:py-8 py-2 flex-col md:min-w-[760px] lg:bottom-[5%] bottom-[0%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-gray-200 border-slate-300 border rounded-xl text-center shadow-xl">
-          <p className="sm:text-2xl font-medium  text-lg mb-6">Data Services</p>
+        <div className="absolute lg:mt-20 lg:py-8 py-2 flex-col md:min-w-[760px] lg:bottom-[5%] bottom-[-25%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-gray-200 border-slate-300 border rounded-xl text-center shadow-xl">
+          <p className="sm:text-2xl font-medium  text-lg lg:mb-6 mb-1">Data Services</p>
           <div className="flex justify-between flex-wrap px-4">
-            <p className="flex px-4 py-2 text-slate-500 font-medium text-lg">
+            <p className="flex px-1 lg:py-2 py-0.5 text-slate-500 font-medium text-lg">
               <CloudUploadIcon className="h-6 text-violet-600" /> App Security
             </p>
-            <p className="flex px-4 py-2 text-slate-500 font-medium text-lg">
+            <p className="flex px-1 lg:py-2 py-0.5 text-slate-500 font-medium text-lg">
               <DatabaseIcon className="h-6 text-violet-600" /> Dashboard Design
             </p>
-            <p className="flex px-4 py-2 text-slate-500 font-medium text-lg">
+            <p className="flex px-1 lg:py-2 py-0.5 text-slate-500 font-medium text-lg">
               <PaperAirplaneIcon className="h-6 text-violet-600" /> Cloud Data
             </p>
-            <p className="flex px-4 py-2 text-slate-500 font-medium text-lg">
+            <p className="flex px-1 lg:py-2 py-0.5 text-slate-500 font-medium text-lg">
               <ServerIcon className="h-6 text-violet-600" /> API
             </p>
           </div>
