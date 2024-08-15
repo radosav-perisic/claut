a392661f493239a4f78c2916f5b7cfd459b0e1fa
Radosav Perisic <radosavperisic00@gmail.com>
2024-08-15 18:29:23 +0200

    Update Server.js
    Set up connecting to MongoDB Atlas using Mongoose

--- user-auth-server/server.js
+++ user-auth-server/server.js
@@ -11,7 +11,11 @@ const app = express();
 app.use(cors()); 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
